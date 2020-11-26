3a3e60751272d84708ac4c993a2b9b5da2c91f33
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-11-26 12:14:53 -0300

    tratamento de error

--- webapi/src/index.js
+++ webapi/src/index.js
@@ -7,54 +7,70 @@ const heroService = HeroFactory.generateInstance()
 const Hero = require('./entities/hero')
 
 const routes = {
-    '/heroes:get': async (request, response) => {
-        const { id } = request.queryString
-        const heroes = await heroService.find(id)
-        response.write(JSON.stringify({ results: heroes }))
-        
-        return response.end()
-    },
-    '/heroes:post': async (request, response) => {
-        // async iterator
-        for await ( const data of request) {
-            const item = JSON.parse(data)
-            const hero = new Hero(item)
-            const { error, valid } = hero.isValid()
-            if (!valid) {
-                response.writeHead(400, DEFAULT_HEADER)
-                response.write(JSON.stringify({ error: error.join(',') }))
-                return response.end()
-            }
+	'/heroes:get': async (request, response) => {
+		const { id } = request.queryString
+		const heroes = await heroService.find(id)
+		response.write(JSON.stringify({ results: heroes }))
 
-            const id = await heroService.create(hero)
-            response.writeHead(201, DEFAULT_HEADER)
-            response.write(JSON.stringify({ success: 'User Created with success!!', id }))
+		return response.end()
+	},
+	'/heroes:post': async (request, response) => {
+		// async iterator
+		for await (const data of request) {
+			try {
 
-            // só jogamos o return aqui pois sabemos que é um objeto body por
-            // requisição se fosse um aquivo, que sobe sobre demanda
-            // ele poderia entrar mais vezes em um mesmo evento, ai removeríamos o return
-            return response.end()
-        }
-    },
+				// await Promise.reject('/heroes:get')
+				const item = JSON.parse(data)
+				const hero = new Hero(item)
+				const { error, valid } = hero.isValid()
+				if (!valid) {
+					response.writeHead(400, DEFAULT_HEADER)
+					response.write(JSON.stringify({ error: error.join(',') }))
+					return response.end()
+				}
 
-    default: (request, response) => {
-        response.write('Hello!!!')
-        response.end()
-    }
+				const id = await heroService.create(hero)
+				response.writeHead(201, DEFAULT_HEADER)
+				response.write(JSON.stringify({ success: 'User Created with success!!', id }))
+
+				// só jogamos o return aqui pois sabemos que é um objeto body por
+				// requisição se fosse um aquivo, que sobe sobre demanda
+				// ele poderia entrar mais vezes em um mesmo evento, ai removeríamos o return
+				return response.end()
+			} catch (error) {
+				return handleError(response)(error)
+			}
+		}
+	},
+
+	default: (request, response) => {
+		response.write('Hello!!!')
+		response.end()
+	}
+}
+
+const handleError = response => {
+	return error => {
+		console.error('Deu Ruim!***', error)
+		response.writeHead(500, DEFAULT_HEADER)
+		response.write(JSON.stringify({ error: 'Internal Server Error!!' }))
+
+		return response.end()
+	}
 }
 
 const handler = (request, response) => {
-    const { url, method } = request
-    const [first, route, id] = url.split('/')
-    request.queryString = { id: isNaN(id) ? id : Number(id) }
-    
-    const key = `/${route}:${method.toLowerCase()}`
-    
-    response.writeHead(200, DEFAULT_HEADER)
+	const { url, method } = request
+	const [first, route, id] = url.split('/')
+	request.queryString = { id: isNaN(id) ? id : Number(id) }
 
-    const chosen = routes[key] || routes.default
-    return chosen(request, response)
+	const key = `/${route}:${method.toLowerCase()}`
+
+	response.writeHead(200, DEFAULT_HEADER)
+
+	const chosen = routes[key] || routes.default
+	return chosen(request, response).catch(handleError(response))
 }
 
 http.createServer(handler)
-    .listen(PORT, () => console.log('server running at', PORT))
+	.listen(PORT, () => console.log('server running at', PORT))
