324f68394e2e60242cd29337dcae7976cc7254f8
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-11-26 11:31:20 -0300

    problema para rodar com nodemon

--- script.sh
+++ script.sh
@@ -1,15 +1,26 @@
-echo '\n\n requesting all heroes'
+#!/bin/bash
+printf '\n\n requesting all heroes'
 curl localhost:3000/heroes
 
-echo '\n\n requesting flash'
+printf '\n\n requesting flash'
 curl localhost:3000/heroes/1
 
-echo '\n\n requesting with wrong body'
+printf '\n\n requesting with wrong body'
 curl --silent -X POST \
     --data-binary '{"invalid": "data"}' \
     localhost:3000/heroes
 
-echo '\n\n creating Leoric'
-curl --silent -X POST \
+printf '\n\n creating Leoric'
+CREATE=$(curl --silent -X POST \
     --data-binary '{"name": "Leoric", "age": 2000, "power": "Cleave Attack"}' \
-    localhost:3000/heroes
+    localhost:3000/heroes)
+
+echo "$CREATE"
+
+ID=$(echo "$CREATE" | jq .id)
+
+# caso de erro na requisição com o nodemon 
+# execute: node webapi/src/index.js
+# e tente novamente.
+printf '\n\n requesting leoric'
+curl localhost:3000/heroes/"$ID"
